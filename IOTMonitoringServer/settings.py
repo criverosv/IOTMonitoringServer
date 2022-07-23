e07a8b9e3013db0fdc7c6724606d2aa0ac627e6e
Carlos Riveros <carlosriverosv@gmail.com>
2022-07-23 12:01:27 -0500

    updating ip addresses

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.157.105.196"]
+ALLOWED_HOSTS = ["localhost", "3.216.30.104"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.235.131.244",  # Dirección IP de la base de datos
+        "HOST": "3.237.80.171",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "44.203.206.122"
+MQTT_HOST = "52.3.231.26"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
